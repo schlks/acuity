2093fc21f29f49d468d2a6a182b2754e19ba1afe
Shlks <lukasschepp10@gmail.com>
2026-06-25 19:56:42 +0200

    clean up of the main function

--- cmd/acuity/main.go
+++ cmd/acuity/main.go
@@ -10,62 +10,32 @@ import (
 
 	"acuity/pkg/db"
 	"acuity/pkg/web"
+	"acuity/internal/config"
 )
 
 func main() {
-	wClient, err := db.NewWeaviateClient("localhost:8080")
+	Config, err := config.Load()
+	if err != nil {
+		log.Fatalf("Failed to load config: %v", err)
+	}
+
+	wClient, err := db.NewWeaviateClient(Config.WeaviateHost)
 	if err != nil {
 		log.Fatalf("Failed to create Weaviate client: %v", err)
 	}
-	sClient, err := db.NewSqliteDB("acuity.db")
+
+	sClient, err := db.NewSqliteDB(Config.DBPath)
 	if err != nil {
 		log.Fatalf("Failed to open SQLite database")
 	}
-	err = sClient.InitTable()
-	if err != nil {
+
+	if err := sClient.InitTable(); err != nil {
 		log.Fatalf("Failed to initialize table")
 	}
 
-	// ctx := context.Background()
-	// wClient.ResetDatabase(ctx)
-
-	err = wClient.InitSchema()
-	if err != nil {
+	if err := wClient.InitSchema(); err != nil {
 		log.Fatalf("Error during schema initialization: %v", err)
 	}
-
-	/*
-	known, err := wClient.GetKnownPaths(ctx)
-	if err != nil {
-		log.Fatalf("Error retrieving existing files: %v", err)
-	}
-	var filePaths []string
-	err = filepath.WalkDir("testdata", func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			return err
-		}
-		absPath, err := filepath.Abs(path)
-		if err != nil {
-			return err
-		}
-		// d.IsDir() filters out directories; remove this check to include folders
-		if !d.IsDir() {
-			if _, exists := known[absPath]; !exists {
-				filePaths = append(filePaths, absPath)
-			}
-		}
-		return nil
-	})
-	if err != nil {
-		log.Printf("Error: %v", err)
-		return
-	}
-
-	go func(){
-		ctx = context.Background()
-		wClient.ImportImages(ctx, filePaths)
-	}()
-	*/
 
 	templates := template.Must(template.ParseGlob("src/web/templates/*.html"))
 
@@ -74,8 +44,8 @@ func main() {
 	mux := http.NewServeMux()
 	webServer.RegisterRoutes(mux)
 
-	log.Println("acuity Web-Interface listening on http://localhost:3000")
-	err = http.ListenAndServe(":3000", mux)
+	log.Printf("acuity Web-Interface listening on http://localhost:%s\n", Config.Port)
+	err = http.ListenAndServe(Config.Port, mux)
 	if err != nil {
 		log.Fatalf("Server crashed: %v", err)
 	}
